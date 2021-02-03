0152a8d9e6c25c9b7e3b6d3b99563a075c26d706
hossen29 <hossenjjjhossen@gmail.com>
2021-02-03 12:53:27 +0600

    new commendt

--- hossen.js
+++ hossen.js
@@ -1,53 +1,55 @@
 
-const plusBtn = document.getElementById("plus-btn");
-plusBtn.addEventListener("click",function(){
-   tasCode(true);
-});
+ const plusBtn = document.getElementById("plus-btn");
+ plusBtn.addEventListener("click",function(){
+    tasCode(true);
+ });
 
-const minusBtn = document.getElementById("minus-btn");
- minusBtn.addEventListener("click",function(){
-   tasCode(false);
-});
+ const minusBtn = document.getElementById("minus-btn");
+  minusBtn.addEventListener("click",function(){
+    tasCode(false);
+ });
 
 
-function tasCode (incrise){
-   const minusKora = document.getElementById("iphone-11").value;
-    const minusKoraInt = parseFloat(minusKora);
-    let minusHoyeGachy = minusKoraInt
-    if (incrise == true){
-        minusHoyeGachy = minusKoraInt + 1;
-    };
-    if(incrise == false && minusKoraInt > 0){
-        minusHoyeGachy = minusKoraInt - 1;
-    }
-   document.getElementById("iphone-11").value = minusHoyeGachy;
-   const minasHisab = minusHoyeGachy * 1219;
-    document.getElementById("many-taka").innerText = minasHisab;
+ function tasCode (incrise){
+    const minusKora = document.getElementById("iphone-11").value;
+     const minusKoraInt = parseFloat(minusKora);
+     let minusHoyeGachy = minusKoraInt
+     if (incrise == true){
+         minusHoyeGachy = minusKoraInt + 1;
+     };
+     if(incrise == false && minusKoraInt > 0){
+         minusHoyeGachy = minusKoraInt - 1;
+     };
+    document.getElementById("iphone-11").value = minusHoyeGachy;
+    const minasHisab = minusHoyeGachy * 1219;
+     document.getElementById("many-taka").innerText = minasHisab;
         
-}
+ };
 
-const plusBtn = document.getElementById("dbbol-plas");
-plusBtn.addEventListener("click",function(){
-   tasCode1(true);
-});
 
-const minusBtn = document.getElementById("dbbol-minas");
- minusBtn.addEventListener("click",function(){
-   tasCode1(false);
-});
+ const plusBtn = document.getElementById("dbbol-plas");
+ plusBtn.addEventListener("click",function(){
+    tasNewCode(true);
+ });
 
-function tasCode1 (decrise){
+ const minusBtn = document.getElementById("dbbol-minas");
+  minusBtn.addEventListener("click",function(){
+    tasNewCode(false);
+ });
+
+
+ function tasNewCode (incrise){
     const minusKora = document.getElementById("phone-cover").value;
      const minusKoraInt = parseFloat(minusKora);
      let minusHoyeGachy = minusKoraInt
-     if (decrise == true){
+    if (incrise == true){
          minusHoyeGachy = minusKoraInt + 1;
      };
-     if(decrise == false && minusKoraInt > 0){
+     if(incrise == false && minusKoraInt > 0){
          minusHoyeGachy = minusKoraInt - 1;
-     }
+     };
     document.getElementById("phone-cover").value = minusHoyeGachy;
     const minasHisab = minusHoyeGachy * 59;
      document.getElementById("cover-tk").innerText = minasHisab;
-         
- }
+        
+ };
